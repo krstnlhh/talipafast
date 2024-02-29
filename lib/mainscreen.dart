6e40ae724f7e11daf45c5da3ba302b460e242184
lheimonade <83322812+lheimonade@users.noreply.github.com>
2024-02-29 15:29:51 +0800

    All in

---
+++ lib/mainscreen.dart
@@ -0,0 +1,113 @@
+//import 'package:talipafast/login_screen.dart';
+import 'package:talipafast/sign-upscreen.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(10.0, 200, 10.0, 0.0),
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                //ignore: prefer_const_constructors
+
+                //logo
+                child: Image.asset(
+                  "assets/tomato.png",
+                  height: 210,
+                ),
+              ),
+
+              const SizedBox(
+                height: 18,
+              ),
+
+              //app title
+              Text(
+                'FindMyDorm',
+                style: TextStyle(
+                  color: Colors.grey[00],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              //login b
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignUpScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  margin: const EdgeInsets.symmetric(horizontal: 100),
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(80),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 5,
+              ),
+
+              //sign up b
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignUpScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(18),
+                  margin: const EdgeInsets.symmetric(horizontal: 100),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(80),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/sign-upscreen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+//import '../screenpages/login_screen.dart';
+//import '../JsonModels/users.dart';
+//import '../SQLite/sqlite.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUpScreen> {
+  final username = TextEditingController();
+  final password = TextEditingController();
+  final confirmPassword = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool isVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //SingleChildScrollView to have an scroll in the screen
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //We will copy the previous textfield we designed to avoid time consuming
+
+                  const ListTile(
+                    title: Text(
+                      "Register New Account",
+                      style:
+                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+
+                  //As we assigned our controller to the textformfields
+
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple.withOpacity(.2)),
+                    child: TextFormField(
+                      controller: username,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username is required";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        border: InputBorder.none,
+                        hintText: "Username",
+                      ),
+                    ),
+                  ),
+
+                  //Password field
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple.withOpacity(.2)),
+                    child: TextFormField(
+                      controller: password,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password is required";
+                        }
+                        return null;
+                      },
+                      obscureText: !isVisible,
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.lock),
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                //In here we will create a click to show and hide the password a toggle button
+                                setState(() {
+                                  //toggle button
+                                  isVisible = !isVisible;
+                                });
+                              },
+                              icon: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))),
+                    ),
+                  ),
+
+                  //Confirm Password field
+                  // Now we check whether password matches or not
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple.withOpacity(.2)),
+                    child: TextFormField(
+                      controller: confirmPassword,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password is required";
+                        } else if (password.text != confirmPassword.text) {
+                          return "Passwords don't match";
+                        }
+                        return null;
+                      },
+                      obscureText: !isVisible,
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.lock),
+                          border: InputBorder.none,
+                          hintText: "Confirm Password",
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                //In here we will create a click to show and hide the password a toggle button
+                                setState(() {
+                                  //toggle button
+                                  isVisible = !isVisible;
+                                });
+                              },
+                              icon: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))),
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+                  //Login button
+                  Container(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width * .9,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.amber),
+                    child: TextButton(
+                        onPressed: () {
+                          //After success user creation go to login screen
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpScreen()));
+                        },
+                        child: const Text(
+                          "SIGN UP",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+
+                  //Sign up button
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?"),
+                      TextButton(
+                          onPressed: () {
+                            //Navigate to sign up
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen()));
+                          },
+                          child: const Text("LOGIN"))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
